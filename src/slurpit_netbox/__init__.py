2601e990e9514d6a742593c66268b05ab152d56d
imi <imi.somogyi@netyce.com>
2023-12-05 14:08:25 +0100

    wip:

--- src/slurpit_netbox/__init__.py
+++ src/slurpit_netbox/__init__.py
@@ -24,11 +24,11 @@ class SlurpitConfig(PluginConfig):
     }
 
     def ready(self):
-        from .models import post_migration
-        dcim_app = apps.get_app_config("dcim")
-        post_migrate.connect(post_migration, sender=dcim_app, weak=False)
-        super().ready()
         try:
+            from .models import post_migration
+            dcim_app = apps.get_app_config("dcim")
+            post_migrate.connect(post_migration, sender=dcim_app, weak=False)
+            super().ready()
             from .models.planning import Planning
             from .views.planning import make_planning_tabs
             planning = Planning.get_planning()
