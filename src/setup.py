b8eadbf218b814826a40b2a8f7d9b996a3386e5b
Indrit <codility22@gmail.com>
2023-12-09 16:15:55 +0800

    adding setup.py for development

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='slurpit-netbox',
+    version='0.1',
+    description="Sync Slurp'IT into NetBox",
+    install_requires=[
+        'arrow', 'netmiko', 'requests', 'httpx'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
