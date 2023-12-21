388bbd705f0f6733bfbc7ed66cf428278f755119
Philip <philipapbak@gmail.com>
2023-12-21 00:48:24 -0500

    Fixed search function on logging page

--- src/slurpit_netbox/filtersets.py
+++ src/slurpit_netbox/filtersets.py
@@ -1,9 +1,9 @@
 import django_filters
 from core.choices import DataSourceStatusChoices
 from django.db.models import Q
-from netbox.filtersets import NetBoxModelFilterSet
-from .models import Source
-
+from netbox.filtersets import NetBoxModelFilterSet, BaseFilterSet
+from .models import Source, SlurpitLog
+from django.utils.translation import gettext as _
 
 class SourceFilterSet(NetBoxModelFilterSet):
     status = django_filters.MultipleChoiceFilter(
@@ -21,6 +21,27 @@ class SourceFilterSet(NetBoxModelFilterSet):
             Q(name__icontains=value)
             | Q(description__icontains=value)
             | Q(comments__icontains=value)
+        )
+
+class LogginFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label=_('Search'),
+    )
+
+    class Meta:
+        model = SlurpitLog
+        fields = [
+            'log_time', 'level', 'category', 'message'
+        ]
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(level__icontains=value) | 
+            Q(category__icontains=value) |
+            Q(message__icontains=value)
         )
 
 
--- src/slurpit_netbox/views/logging.py
+++ src/slurpit_netbox/views/logging.py
@@ -1,8 +1,10 @@
 from netbox.views import generic
 from ..models import SlurpitLog
 from ..tables import LoggingTable
+from ..filtersets import LogginFilterSet
 
 class LoggingListView(generic.ObjectListView):
     queryset = SlurpitLog.objects.all()
+    filterset = LogginFilterSet
     table = LoggingTable
     template_name = "slurpit_netbox/slurpitlog_list.html"
