75e2106a9ddcc4dc2748172d8d8d5784cf26c55f
Philip <philipapbak@gmail.com>
2024-01-11 09:17:47 -0500

    Fixed serializer

--- src/slurpit_netbox/api/serializers.py
+++ src/slurpit_netbox/api/serializers.py
@@ -1,8 +1,18 @@
+from core.choices import DataSourceStatusChoices
+from netbox.api.fields import ChoiceField
 from netbox.api.serializers import NetBoxModelSerializer
-from slurpit_netbox.models import SlurpitPlan
+from rest_framework import serializers
+
+from slurpit_netbox.models import SlurpitPlan, ImportedDevice, Planning, StagedDevice, Source, SlurpitLog, Setting
 
 __all__ = (
     'SlurpitPlanSerializer',
+    'StagedDeviceSerializer',
+    'ImportedDeviceSerializer',
+    'SourceSerializer',
+    'PlanningSerializer',
+    'SlurpitLogSerializer',
+    'SettingSerializer',
 )
 
 class SlurpitPlanSerializer(NetBoxModelSerializer):
@@ -10,3 +20,50 @@ class SlurpitPlanSerializer(NetBoxModelSerializer):
     class Meta:
         model = SlurpitPlan
         fields = ["id", "name", "plan_id", "display"]
+
+class StagedDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StagedDevice
+        fields = '__all__'
+
+
+class ImportedDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImportedDevice
+        fields = '__all__'
+
+
+class SourceSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="core-api:datasource-detail")
+    status = ChoiceField(choices=DataSourceStatusChoices, read_only=True)
+
+    class Meta:
+        model = Source
+        fields = [
+            "id",
+            "url",
+            "display",
+            "name",
+            "status",
+            "description",
+            "comments",
+            "parameters",
+            "created",
+            "last_updated",
+        ]
+
+
+class PlanningSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = Planning
+        fields = '__all__'
+
+class SlurpitLogSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = SlurpitLog
+        fields = '__all__'
+
+class SettingSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = Setting
+        fields = '__all__'
