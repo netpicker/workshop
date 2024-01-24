ad1368c5e5136d036cc41bb9298fe4c23964c89b
Karel <noppes3000@gmail.com>
2024-01-24 11:33:49 +0100

    missed an arrow reference

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.1.66',
+    version='0.1.67',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'httpx'
