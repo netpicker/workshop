d47eb183bc140ad7bc1d020ad012f0bdf15d2b55
Karel <noppes3000@gmail.com>
2024-01-24 11:36:25 +0100

    httpx wasnt used anywhere

--- src/setup.py
+++ src/setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.1.67',
+    version='0.1.68',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
-        'requests', 'httpx'
+        'requests', 'djangorestframework-bulk'
     ],
     packages=find_packages(),
     include_package_data=True,
