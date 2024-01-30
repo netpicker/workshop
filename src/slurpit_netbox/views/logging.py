085d2770bd08a8e4e4b5d49eea77790fafc1ab25
Philip <philipapbak@gmail.com>
2024-01-30 09:39:47 -0500

    logs sort is fixed

--- src/slurpit_netbox/views/logging.py
+++ src/slurpit_netbox/views/logging.py
@@ -8,7 +8,7 @@ from django.utils.decorators import method_decorator
 
 @method_decorator(slurpit_plugin_registered, name='dispatch')
 class LoggingListView(generic.ObjectListView):
-    queryset = SlurpitLog.objects.all()
+    queryset = SlurpitLog.objects.all().order_by("-log_time")
     filterset = LoggingFilterSet
     table = LoggingTable
     template_name = "slurpit_netbox/slurpitlog_list.html"
