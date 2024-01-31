287dcbaf6fe19204e87ec90312622bb8bce7ef7d
Karel <noppes3000@gmail.com>
2024-01-31 09:19:01 +0100

    pypi build test

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.8.32',
+    version='0.8.33',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
