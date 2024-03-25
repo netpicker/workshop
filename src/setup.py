33f760e0b87e4d03d79a1920dc91d22a47197d59
Philip <philipapbak@gmail.com>
2024-03-25 03:11:33 -0400

    rename the app type cloud to push

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.8.77',
+    version='0.8.78',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
--- src/slurpit_netbox/decorators.py
+++ src/slurpit_netbox/decorators.py
@@ -45,7 +45,7 @@ def slurpit_plugin_registered(view_func):
 
                     if appliance_type == '':
                         messages.warning(request, "To use the Slurp'it plugin, it is necessary to first select the Appliance Type on the Setting Page.")
-                    elif appliance_type != 'cloud' and (api_key == '' or server_url == ''):
+                    elif appliance_type != 'push' and (api_key == '' or server_url == ''):
                         messages.warning(request, "To use the Slurp'it plugin, you should first configure the server settings. Go to settings and configure the Slurp'it server in the parameter section.")
                 except Exception as e:
                     setting = None
