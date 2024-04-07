4dd9ecac66d954926fa3cf3803fac294a77c0843
Philip <philipapbak@gmail.com>
2024-04-07 04:29:38 +0200

    Test devices

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -101,3 +101,58 @@ def test_plannings(setup):
 
     assert len(get_plannings()) == 0
 
+
+def compare_devices(array1, array2):
+    assert len(array1) == len(array2), f"Device arrays were different sizes: {array1} - {array2}"
+
+    for row in array1:
+        result = next((obj for obj in array2 if obj['slurpit_id'] == row['id']), None)
+        assert result is not None, f"Unable to find slurpit_id {row['id']}"
+        assert row['hostname'] == result['hostname']
+        assert row['fqdn'] == result['fqdn']
+        assert row['device_os'] == result['device_os']
+        assert row['device_type'] == result['device_type']
+        assert row['brand'] == result['brand']
+        assert row['ipv4'] == result['ipv4']
+
+def get_devices():
+    with connection() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM slurpit_netbox_slurpitstageddevice")
+        return cur.fetchall()
+
+def test_devices(setup):
+    devices = [{
+        "id": "1",
+        "hostname": "MikroTikAB",
+        "fqdn": "WWW",
+        "ipv4": "192.168.34.56",
+        "device_os": "mellanox",
+        "device_type": "RB1200",
+        "brand": "Mellanox Technologies",
+        "disabled": "0",
+        "createddate": "2023-11-27 14:05:26",
+        "changeddate": "2024-04-04 16:27:23"
+    }]
+    response = do_request('device/', method="POST",data=devices)
+    assert response.status_code == 200
+    assert response.json()['status'] == "success"
+
+    compare_devices(devices, get_devices())
+
+    response = do_request('device/sync_start/', method="POST")
+    assert response.status_code == 200
+    assert response.json()['status'] == "success"
+
+    response = do_request('device/sync/', method="POST", data=devices)
+    assert response.status_code == 200
+    assert response.json()['status'] == "success"
+
+
+    response = do_request('device/sync_end/', method="POST")
+    assert response.status_code == 200
+    assert response.json()['status'] == "success"
+
+    compare_devices(devices, get_devices())
+
+    # Check if Platform & Manufacturer & Device Type are all created on slurpit.
+    
