b3cd0eb5288de484ce8fb823906cde907a12701b
Philip <philipapbak@gmail.com>
2024-06-13 11:59:38 -0400

    Fixed to refresh model on reconcile page

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.9.17',
+    version='0.9.18',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
