09ea08392d9fd814b58bceff8ced3cb7aa2ad2db
Philip <philipapbak@gmail.com>
2024-06-20 03:28:52 -0400

    Remove action button on reconcile page

--- src/slurpit_netbox/views/reconcile.py
+++ src/slurpit_netbox/views/reconcile.py
@@ -35,6 +35,7 @@ class ReconcileView(generic.ObjectListView):
     table = tables.SlurpitIPAMTable
     template_name = "slurpit_netbox/reconcile.html"
     filterset = SlurpitIPAddressFilterSet
+    action_buttons = []
     
     def get(self, request, *args, **kwargs):
         
