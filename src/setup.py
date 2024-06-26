1857a0604770d4cd77bbfda41805835da2a2415f
Philip <philipapbak@gmail.com>
2024-06-26 04:04:25 -0400

    update version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.9.30',
+    version='0.9.31',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
