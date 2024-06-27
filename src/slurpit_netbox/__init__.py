2b5d0778b152e3067de06d622efa2b5c66970264
Pieter <phvanos@gmail.com>
2024-06-27 16:46:15 +0200

    removed disabled btn code

--- src/slurpit_netbox/__init__.py
+++ src/slurpit_netbox/__init__.py
@@ -8,7 +8,7 @@ class SlurpitConfig(PluginConfig):
     name = "slurpit_netbox"
     verbose_name = "Slurp'it Plugin"
     description = "Sync Slurp'it into NetBox"
-    version = '0.9.34'
+    version = '0.9.35'
     base_url = "slurpit"   
     default_settings = {
         'DeviceType': {'model': "Slurp'it", 'slug': 'slurpit'},
