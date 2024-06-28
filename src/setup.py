190bc5138e7ad40a91b70b23b5adbc7b70918e2d
Philip <philipapbak@gmail.com>
2024-06-28 10:29:08 -0400

    Removed config table button

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.9.38',
+    version='0.9.39',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
