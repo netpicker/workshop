29a0f344d1f558a0b248aed7fdfd59baa24251e1
Philip <philipapbak@gmail.com>
2024-07-03 05:27:07 -0400

    Fixed slurpitimporteddevice_list issue on device type page

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.9.42',
+    version='0.9.43',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
