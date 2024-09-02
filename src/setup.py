babf6a205518f1142da73b93bbe5f4b07e7800ad
Philip <philipapbak@gmail.com>
2024-09-02 08:25:26 -0400

    Removed the ignore vrf on mapping page

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slurpit_netbox',
-    version='0.9.71',
+    version='0.9.72',
     description="Sync Slurp'IT into NetBox",
     install_requires=[
         'requests', 'djangorestframework-bulk'
